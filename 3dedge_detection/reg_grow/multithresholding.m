76f6e52b863f537cb275f49943cb2f576305477e
amelinska <aleksandra.melinska@gmail.com>
2013-12-17 10:55:32 +0100

    region growing for 2d improved
    Earlier version of region growing was incorrect. The main function
    run_region_growing_on_slice was improved and prepared to show Robert the
    tested function.

--- 3dedge_detection/reg_grow/multithresholding.m
+++ 3dedge_detection/reg_grow/multithresholding.m
@@ -1,11 +1,11 @@
 function [segemented_image,rgb, gray_segmented_image ] = multithresholding(image, level_num)
 
-thresh = multithresh(image,level_num)
+thresh = multithresh(image,level_num);
 segemented_image = imquantize(image, thresh);
 rgb = label2rgb(segemented_image);
 gray_segmented_image = rgb2gray(rgb);
 %figure, imshowpair(image,segemented_image,'montage')
-figure, imshowpair(image, rgb,'montage')
+%figure, imshowpair(image, rgb,'montage')
 
 
 
